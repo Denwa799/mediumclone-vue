f897f4590005b4b359e5002023285a036286e411
Denwa799 <denwa799@yandex.ru>
2023-01-20 16:36:50 +0300

    Добавляет основную разметку главной страницы

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,28 +1,30 @@
 <template>
     <nav class="navbar navbar-light">
         <div class="container">
-            <router-link class="navbar-brand" :to="{ name: 'home' }">
-                Vue
+            <router-link class="navbar-brand" :to="{ name: 'globalFeed' }">
+                MediumClone
             </router-link>
             <ul class="nav navbar-nav pull-xs-right">
                 <li class="nav-item">
-                    <router-link class="nav-link" :to="{ name: 'home' }" exact active-class="active">
-                        Home
-                    </router-link>
+                    <router-link class="nav-link" :to="{ name: 'globalFeed' }">Home</router-link>
                 </li>
                 <template v-if="isLoggedIn">
                     <li class="nav-item">
-                        <router-link class="nav-link" to="createArticle" active-class="active">
-                            <i class="ion-compose"></i> &nbsp; New Article
+                        <router-link class="nav-link" :to="{ name: 'createArticle' }">
+                            <i class="ion-compose" />
+                            &nbsp; New Article
                         </router-link>
                     </li>
+
                     <li class="nav-item">
-                        <router-link class="nav-link" to="settings" active-class="active">
-                            <i class="ion-gear-a"></i> &nbsp; Settings
+                        <router-link class="nav-link" :to="{ name: 'settings' }">
+                            <i class="ion-gear-a" />
+                            &nbsp; Settings
                         </router-link>
                     </li>
+
                     <li class="nav-item">
-                        <router-link class="nav-link" to="userProfile" active-class="active">
+                        <router-link class="nav-link" :to="{ name: 'userProfile', params: { slug: currentUser.username } }">
                             <img class="user-pic" :src="currentUser.image" />
                             &nbsp;
                             {{ currentUser.username }}
@@ -31,12 +33,12 @@
                 </template>
                 <template v-if="isAnonymous">
                     <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'login' }" active-class="active">
+                        <router-link class="nav-link" :to="{ name: 'login' }">
                             Sign in
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'register' }" active-class="active">
+                        <router-link class="nav-link" :to="{ name: 'register' }">
                             Sign up
                         </router-link>
                     </li>
@@ -51,7 +53,7 @@ import { mapGetters } from 'vuex'
 import { getterTypes } from '@/store/modules/auth'
 
 export default {
-    name: 'AppTopbar',
+    name: 'McvTopbar',
     computed: {
         ...mapGetters({
             currentUser: getterTypes.currentUser,
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,9 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import Home from '@/views/Home'
+import GlobalFeed from '@/views/GlobalFeed'
 import Register from '@/views/Register'
 import Login from '@/views/Login'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
   {
     path: '/register',
     name: 'register',
@@ -18,6 +13,51 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login,
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: GlobalFeed,
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: GlobalFeed,
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeed,
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeed,
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeed,
   },
 ]
 
